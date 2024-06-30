a44dfc7b8ad96ba0a2e7f149489499446c189ec6
DomenicWalther <domenicwalthercreatiiv@gmail.com>
2024-06-30 14:15:49 +0200

    fix: bug where user address wasnt properly loaded

--- src/main/mailer.ts
+++ src/main/mailer.ts
@@ -1,44 +1,57 @@
 import nodemailer from 'nodemailer'
-import { getSettings } from "./settings"
+import { getSettings } from './settings'
 import 'dotenv/config'
-
-
-
 
 async function setMailSettings() {
   try {
-    let userSettings = await getSettings("anmeldedaten")
+    let userSettings = await getSettings('anmeldedaten')
     let transporter = nodemailer.createTransport({
       host: 'securesmtp.t-online.de',
       port: 465,
       secure: true,
       pool: true,
       auth: {
-        user: userSettings.emailAdress,
+        user: userSettings.emailAddress,
         pass: userSettings.password
       }
     })
-    return { transporter: transporter, emailSender: userSettings.emailAdress, emailText: userSettings.emailText }
+    return {
+      transporter: transporter,
+      emailSender: userSettings.emailAdress,
+      emailText: userSettings.emailText
+    }
   } catch (error) {
-    throw (error)
+    throw error
   }
 }
-export async function main(downloadLink: string, emailReceiver: string, mainWindow: Electron.BrowserWindow) {
+export async function main(
+  downloadLink: string,
+  emailReceiver: string,
+  mainWindow: Electron.BrowserWindow
+) {
   let { transporter, emailSender, emailText } = await setMailSettings()
-  transporter.sendMail(createMessage(downloadLink, emailReceiver, emailSender, emailText), function(error, info) {
-    if (error) {
-      mainWindow.webContents.send('mail-error', error)
-    } else {
-      mainWindow.webContents.send('mail-sent', info.response)
+  transporter.sendMail(
+    createMessage(downloadLink, emailReceiver, emailSender, emailText),
+    function (error, info) {
+      if (error) {
+        mainWindow.webContents.send('mail-error', error)
+      } else {
+        mainWindow.webContents.send('mail-sent', info.response)
+      }
     }
-  })
+  )
 }
 
-function createMessage(downloadLink: string, emailReceiver: string, emailSender: string, emailText: string) {
+function createMessage(
+  downloadLink: string,
+  emailReceiver: string,
+  emailSender: string,
+  emailText: string
+) {
   return {
     from: emailSender,
     to: emailReceiver,
     subject: 'Kindergarten Downloadlink',
-    text: emailText.replace("{download}", downloadLink),
+    text: emailText.replace('{download}', downloadLink)
   }
 }
