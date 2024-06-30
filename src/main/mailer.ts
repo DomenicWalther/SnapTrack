14b9c24f76e20875e2bd2f7e506a5487841abf83
DomenicWalther <domenicwalthercreatiiv@gmail.com>
2024-06-30 14:24:38 +0200

    fix: reuse same transporter for every mail instead of creating a new connection for each one

--- src/main/mailer.ts
+++ src/main/mailer.ts
@@ -1,57 +1,61 @@
-import nodemailer from 'nodemailer'
-import { getSettings } from './settings'
-import 'dotenv/config'
+import nodemailer from "nodemailer";
+import { getSettings } from "./settings";
+import "dotenv/config";
+
+let transporter: nodemailer.transporter;
 
 async function setMailSettings() {
-  try {
-    let userSettings = await getSettings('anmeldedaten')
-    let transporter = nodemailer.createTransport({
-      host: 'securesmtp.t-online.de',
-      port: 465,
-      secure: true,
-      pool: true,
-      auth: {
-        user: userSettings.emailAddress,
-        pass: userSettings.password
-      }
-    })
-    return {
-      transporter: transporter,
-      emailSender: userSettings.emailAdress,
-      emailText: userSettings.emailText
-    }
-  } catch (error) {
-    throw error
-  }
+	try {
+		const userSettings = await getSettings("anmeldedaten");
+		if (!transporter) {
+			transporter = nodemailer.createTransport({
+				host: "securesmtp.t-online.de",
+				port: 465,
+				secure: true,
+				pool: true,
+				auth: {
+					user: userSettings.emailAddress,
+					pass: userSettings.password,
+				},
+			});
+		}
+		return {
+			transporter: transporter,
+			emailSender: userSettings.emailAddress,
+			emailText: userSettings.emailText,
+		};
+	} catch (error) {
+		throw error;
+	}
 }
 export async function main(
-  downloadLink: string,
-  emailReceiver: string,
-  mainWindow: Electron.BrowserWindow
+	downloadLink: string,
+	emailReceiver: string,
+	mainWindow: Electron.BrowserWindow,
 ) {
-  let { transporter, emailSender, emailText } = await setMailSettings()
-  transporter.sendMail(
-    createMessage(downloadLink, emailReceiver, emailSender, emailText),
-    function (error, info) {
-      if (error) {
-        mainWindow.webContents.send('mail-error', error)
-      } else {
-        mainWindow.webContents.send('mail-sent', info.response)
-      }
-    }
-  )
+	let { transporter, emailSender, emailText } = await setMailSettings();
+	transporter.sendMail(
+		createMessage(downloadLink, emailReceiver, emailSender, emailText),
+		(error, info) => {
+			if (error) {
+				mainWindow.webContents.send("mail-error", error);
+			} else {
+				mainWindow.webContents.send("mail-sent", info.response);
+			}
+		},
+	);
 }
 
 function createMessage(
-  downloadLink: string,
-  emailReceiver: string,
-  emailSender: string,
-  emailText: string
+	downloadLink: string,
+	emailReceiver: string,
+	emailSender: string,
+	emailText: string,
 ) {
-  return {
-    from: emailSender,
-    to: emailReceiver,
-    subject: 'Kindergarten Downloadlink',
-    text: emailText.replace('{download}', downloadLink)
-  }
+	return {
+		from: emailSender,
+		to: emailReceiver,
+		subject: "Kindergarten Downloadlink",
+		text: emailText.replace("{download}", downloadLink),
+	};
 }
